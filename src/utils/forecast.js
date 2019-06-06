7be7fb7eacbee16ff55a7367e61662b4c60c02a5
Manmeet Gill <msgillus@gmail.com>
2019-06-06 02:34:47 -0700

    added new info humidity and wind speed

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,15 +1,17 @@
  const request = require('request')
 
+
+
  var forecast = (lat, lon, callback) =>{
     const url = 'https://api.darksky.net/forecast/a50dee52270f4816bdd2b5bb1786ca90/' + lat+','+lon
 
-    request({ url, json: true}, (error, response)=>{
+    request({ url, json: true}, (error, {body}={})=>{
         if(error){
             callback('Unable to connect to Network services', undefined)
-        }else if(response.body.error){
+        }else if(body.error){
             callback('Unable to get weather for location, try again with different location', undefined)
         } else{
-            callback(undefined, response.body.daily.data[0].summary+'it is currently '+ response.body.currently.temperature+' degrees out. There is a '+ response.body.currently.precipProbability+'% chance of rain.')
+            callback(undefined, body.daily.data[0].summary+'it is currently '+ body.currently.temperature+' degrees out. There is a '+ body.currently.precipProbability+'% chance of rain. \nHumidity is approx. '+body.currently.humidity*100 +'% with wind speed of '+body.currently.windSpeed+' miles per hour.')
         }
     })
 }
